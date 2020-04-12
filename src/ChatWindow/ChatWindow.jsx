e4ab5d73d5593f5b5f43034d52e2d75fbb0343c3
subhaoi <subhaoi@gmail.com>
2020-04-12 11:52:53 +0530

    testing http requests

--- src/ChatWindow/ChatWindow.jsx
+++ src/ChatWindow/ChatWindow.jsx
@@ -1,25 +1,152 @@
 import React, { Component } from 'react';
 import { AppNavBar } from '../AppNavBar';
 import { Button, Container, Row, Col} from 'react-bootstrap';
+import 'react-chatbox-component/dist/style.css';
+import {ChatBox} from 'react-chatbox-component';  
+import axios from 'axios';
+import { number } from 'prop-types';
 
 class ChatWindow extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      messages: [],
+      user: {},
+      phone_number: number
+    };
+    this.newSubmit = this.onSubmit.bind(this);
+  }
+  handleKeyPress = (event) => {
+    if(event.key === 'Enter'){
+      console.log('enter press here! ')
+    }
+  }
+   onSubmit = function(message){
+    var joined = this.state.messages.concat({
+      "text": message,
+      "id": "9",
+      "sender": {
+        "name": "Ironman",
+        "uid": "user1",
+        "avatar": "https://data.cometchat.com/assets/images/avatars/ironman.png",
+      },
+    })
+    this.setState({ messages: joined })
+    console.log(message);
+
+    axios({
+      method: 'post',
+      url: 'http://a2f25d0b.ngrok.io/message_sent_by_user',
+      data: {
+        'conversation_id': 110,
+        'body': message , 
+        'phone_number': this.state.phone_number
+      },
+      headers:{
+        "Content-Type" : "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      
+    })
+      .then(function (response) {
+        console.log(response)
+      });
+
+    
+  }
+
+
+  componentDidMount() {
+    
+    const messages = [
+      {
+        "text": "Hello there",
+        "id": "1",
+        "sender": {
+          "name": "Ironman",
+          "uid": "user1",
+          "avatar": "https://data.cometchat.com/assets/images/avatars/ironman.png",
+        },
+      },
+      {
+        "text": "Hi Mr. Stark",
+        "id": "2",
+        "sender": {
+          "name": "Spiderman",
+          "uid": "user2",
+          "avatar": "https://data.cometchat.com/assets/images/avatars/spiderman.png",
+        },
+      },
+      {
+        "text": "Hello Stark, how are you today?",
+        "id": "3",
+        "sender": {
+          "name": "Spiderman",
+          "uid": "user2",
+          "avatar": "https://data.cometchat.com/assets/images/avatars/spiderman.png",
+        },
+      },
+      {
+        "text": "Hello Stark, how are you today?",
+        "id": "4",
+        "sender": {
+          "name": "Spiderman",
+          "uid": "user2",
+          "avatar": "https://data.cometchat.com/assets/images/avatars/spiderman.png",
+        },
+      },
+      {
+        "text": "Hello Stark, how are you today?",
+        "id": "5",
+        "sender": {
+          "name": "Spiderman",
+          "uid": "user2",
+          "avatar": "https://data.cometchat.com/assets/images/avatars/spiderman.png",
+        },
+      },
+      {
+        "text": "Hello Stark, how are you today?",
+        "id": "6",
+        "sender": {
+          "name": "Spiderman",
+          "uid": "user2",
+          "avatar": "https://data.cometchat.com/assets/images/avatars/spiderman.png",
+        },
+      },
+      {
+        "text": "Hello Stark, how are you today?",
+        "id": "7",
+        "sender": {
+          "name": "Spiderman",
+          "uid": "user2",
+          "avatar": "https://data.cometchat.com/assets/images/avatars/spiderman.png",
+        },
+      },
+      {
+        "text": "Hello Stark, how are you today?",
+        "id": "8",
+        "sender": {
+          "name": "Spiderman",
+          "uid": "user2",
+          "avatar": "https://data.cometchat.com/assets/images/avatars/spiderman.png",
+        },
+      },
+    ];
+
+    const user = {
+      "uid": "user1"
+    };
+
+    this.setState({ messages: messages, user: user });
+
   }
 
   render() {
     return (
-      <div>
-        <Container fluid>
-          <Row>
-            <Col>
-              <Button>ChatBox</Button>
-            </Col>
-          </Row>
-        </Container>
+      <div className='container' style={{maxWidth: '1000px', paddingTop: '50px'}}>
+        <ChatBox messages={this.state.messages} onKeyPress={this.handleKeyPress} />
       </div>
-    );
+    )
   }
 }
 
--- src/SideBar/SideBar.jsx
+++ src/SideBar/SideBar.jsx
@@ -1,12 +1,109 @@
 import React, { Component } from 'react';
 import { Button, Container, Row, Col, Media, Image, p, span, Glyphicon} from 'react-bootstrap';
+import Table from './Table.jsx';
+import axios from 'axios';
+import './SideBar.css'
+
 
 class SideBar extends Component{
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+          tableData:[
+            {'user': 'user1', 'read': 'no'}],};
+        
+        this.phoneNumberClicked = this.phoneNumberClicked.bind(this);
+        this.openConversation = this.openConversation.bind(this);
+        this.closeConversation = this.closeConversation.bind(this);
+
       }
+      componentDidMount() {
+        axios({
+          method: 'post',
+          url: 'http://a2f25d0b.ngrok.io/all_phone_numbers_by_username',
+          data :{
+            username: 'test_user@test_user.com'
+          },
+          headers:{
+            'Content-Type' : 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          }
+        }).then(function (response) {
+            console.log(response)
+          });
+
+          axios({
+            method: 'post',
+            url: 'http://a2f25d0b.ngrok.io/conversation_opened_by_user',
+            data: {
+              "conversation_id": 110
+            },
+            headers:{
+              "Content-Type" : "application/json",
+              "Access-Control-Allow-Origin": "*",
+            },
+            
+          })
+            .then(function (response) {
+              console.log(response)
+            });
     
+      }
+
+      phoneNumberClicked = function(){ axios({
+        method: 'post',
+        url: 'http://a2f25d0b.ngrok.io/all_messages_for_phone_number_and_username',
+        data: {
+          "username": "test_user@test_user.com",
+          "phone_number": this.state.clicked_number
+        },
+        headers:{
+          "Content-Type" : "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        
+      })
+        .then(function (response) {
+          console.log(response)
+        });
+      }
+
+      openConversation = function(){axios({
+        method: 'post',
+        url: 'http://a2f25d0b.ngrok.io/conversation_opened_by_user',
+        data: {
+          "conversation_id": 110
+        },
+        headers:{
+          "Content-Type" : "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        
+      })
+        .then(function (response) {
+          console.log(response)
+        });
+
+      }
+
+      closeConversation = function(){axios({
+        method: 'post',
+        url: 'http://a2f25d0b.ngrok.io/conversation_closed_by_user',
+        data: {
+          "conversation_id": 110
+        },
+        headers:{
+          "Content-Type" : "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        
+      })
+        .then(function (response) {
+          console.log(response)
+        });
+
+      }
+
       render() {
         return (
             <div>
@@ -22,7 +119,7 @@ class SideBar extends Component{
                       </Col>
                   </Row>
                   <Row>
-                      
+                    <Table data={this.state.tableData}/>
                   </Row>
                 </Container>
             </div>
